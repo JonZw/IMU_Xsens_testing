3e6925beef7ea1c9c3308134acd055bc23316423
Jonas <ty222@uni-heidelberg.de>
2022-06-06 18:34:30 +0200

    Add comments and pseudo code to tbam_comp()

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import mtdevice
-
+import math
 
 # TODO:
 #  Implement the following
 #  1.) Postprocessed Dataset reading
 #  2.) Computation of CoM
 #  3.) Computation of Moment of Inertia
-#  4.) Computation of Total Body Angular Momentum (2 functions: one for linear(?), one for angular momentum)
+#  4.) Computation of Total Body Angular Momentum (one for angular momentum of CoM enough?)
 #  5.) Functions which compare the output of 2 CoM, MoI and TBAM
 #  Split the function in more scripts, if more readable
 #  Note: Apparently a lot of code needed, maybe I should just implement the computation and not the whole thing
@@ -34,7 +34,7 @@ def moi_comp():
     pass
 
 
-def tbam_comp():
+def tbam_comp(): # needs as input the angular velocity of x,y,z and the number of datapoints
     # Angular Momentum = (moment of inertia)(angular velocity) angular velocity can be extracted out of Xsens and
     # Qualisys (Xsens has angular velocity, Qualisys can output 6 DOF)
     # TODO:
@@ -42,8 +42,17 @@ def tbam_comp():
     #  - quaternions,
     #  - euler angles and
     #  - rotation matrix
-    #  2.) Output it in a way of return for comparison (print/diagram + return)
-    pass
+    #  -> Both have Euler and rotation matrix! Try Euler first
+    #  2.) Output it in a way of return for comparison (print/diagram + return) -> Is the angular momentum of CoM ok?
+    #  Formula will be: L = Ic * w, Ic = - sum[i=1; N](m_i * (delta r_i)^2) and w = angular velocity (given from data),
+    #  m_i = mass of i and r_i = denotes the trajectory of each particle i
+    """
+    I_c = 0
+    for i in range(1,N):
+        I_c += mass_i * (delta-r_i)^2
+    I_c = - sum(1, N)
+
+    L = I_c * w"""
 
 
 if __name__ == '__main__':
