a4cb20f8c87338145352e31640b9b6645028f66d
JonZw <jonas.zwack@gmx.de>
2022-06-06 10:36:07 +0200

    Wrote TODO list in main

---
+++ main.py
@@ -0,0 +1,22 @@
+# TODO:
+#  Implement the following
+#  1.) Postprocessed Dataset reading
+#  2.) Computation of CoM
+#  3.) Computation of Moment of Inertia
+#  4.) Computation of Total Body Angular Momentum (2 functions: one for linear(?), one for angular momentum)
+#  Split the function in more scripts, if more readable
+#  Note: Apparently a lot of code needed, maybe I should just implement the computation and not the whole thing
+def in_stream():
+    pass
+
+
+def com_comp():
+    pass
+
+
+def moi_comp():
+    pass
+
+
+def tbam_comp():
+    pass
